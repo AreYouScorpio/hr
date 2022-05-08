12e2932e2edcc11d6e5be10c9a77a2e6ed61db34
AreYouScorpio <akos.schweger@gmail.com>
2022-05-08 13:34:07 +0200

    Initial commit - HR project - SpringBootCourse

---
+++ src/main/java/hu/webuni/hr/akostomschweger/model/Employee.java
@@ -0,0 +1,59 @@
+package hu.webuni.hr.akostomschweger.model;
+
+import java.time.LocalDateTime;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String position;
+    private int salary;
+    private LocalDateTime startDateAtTheCompany;
+
+    public Employee(Long id, String name, String position, int salary, LocalDateTime startDateAtTheCompany) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+        this.startDateAtTheCompany = startDateAtTheCompany;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public LocalDateTime getStartDateAtTheCompany() {
+        return startDateAtTheCompany;
+    }
+
+    public void setStartDateAtTheCompany(LocalDateTime startDateAtTheCompany) {
+        this.startDateAtTheCompany = startDateAtTheCompany;
+    }
+}
