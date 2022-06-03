d3be3e9ab7439c1bcedad8cf6e5328e22c9c4fa6
AreYouScorpio <akos.schweger@gmail.com>
2022-06-03 16:48:02 +0200

    IT test for creating done

--- src/test/java/hu/webuni/hr/akostomschweger/web/EmployeeControllerIT.java
+++ src/test/java/hu/webuni/hr/akostomschweger/web/EmployeeControllerIT.java
@@ -9,15 +9,24 @@ import hu.webuni.hr.akostomschweger.service.EmployeeSuperClass;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@ActiveProfiles({"prod", "test"}) // profile switcher: both profiles on
 public class EmployeeControllerIT {
+
+        private static final String BASE_URI = "/api/employees";
+
+        @Autowired
+        WebTestClient webTestClient;
 
         @Autowired
         EmployeeService employeeService;
@@ -27,6 +36,7 @@ public class EmployeeControllerIT {
 
         // ez csak egy plusz teszt a Service-en át --->
 
+        /*
         @Test
         void testCreateEmployeeValidwithEmployeeService() throws Exception {
                 // service-vel gyártva, Service teszt
@@ -39,29 +49,123 @@ public class EmployeeControllerIT {
 
         }
 
-        // itt a post / create tesztek Controlleren át --->
 
-        @Test // controllerrel gyártva
+         */
+
+
+        @Test
         void testCreateEmployeeValidWithController() throws Exception {
                 // EmployeeController employeeController = new EmployeeController();
-                EmployeeDto employee=
-                        new EmployeeDto(21L,"Akos", "jsj", 200,
-                                LocalDateTime.of(2017, Month.FEBRUARY,3,6,30,40,50000));
-                employeeController.createEmployee(employee);
-                Employee employeeTest = employeeService.findById(21L);
-                assertThat(employeeTest.getSalary()).isEqualTo(200);
+
+                List<EmployeeDto> employeesBefore = getAllEmployees();
+
+                EmployeeDto employee =
+                        new EmployeeDto(21L, "Akos", "jsj", 200,
+                                LocalDateTime.of(2017, Month.FEBRUARY, 3, 6, 30));
+                // employeeController.createEmployee(employee);
+
+                createEmployee(employee);
+
+                List<EmployeeDto> employeesAfter = getAllEmployees();
+
+                assertThat(employeesAfter.subList(0, employeesBefore.size()))
+                        .usingRecursiveFieldByFieldElementComparator()
+                        .containsExactlyElementsOf(employeesBefore);
+
+                assertThat(employeesAfter
+                        .get(employeesAfter.size() - 1))
+                        .usingRecursiveComparison()
+                        .isEqualTo(employee);
+
+
+                // Employee employeeTest = employeeService.findById(21L);
+                //assertThat(employeeTest.getSalary()).isEqualTo(200);
+
 
         }
 
-        @Test // controllerrel gyártva
+        private void createEmployee(EmployeeDto employee) {
+                webTestClient
+                        .post()
+                        .uri(BASE_URI)
+                        .bodyValue(employee)
+                        .exchange()
+                        .expectStatus()
+                        .isOk();
+        }
+
+        private List<EmployeeDto> getAllEmployees() {
+                List<EmployeeDto> responseList =
+                        webTestClient
+                                .get()
+                                .uri(BASE_URI)
+                                .exchange()
+                                .expectStatus()
+                                .isOk()
+                                .expectBodyList(EmployeeDto.class)
+                                .returnResult()
+                                .getResponseBody();
+
+                Collections.sort(responseList, (a1, a2) -> Long.compare(a1.getId(), a2.getId()));
+                return responseList;
+
+        }
+
+
+
+        @Test
         void testCreateEmployeeInvalidWithController() throws Exception {
                 // EmployeeController employeeController = new EmployeeController();
+
+                List<EmployeeDto> employeesBefore = getAllEmployees();
+
                 EmployeeDto employee=
-                        new EmployeeDto(21L,"", "jsj", 200,
+                        new EmployeeDto(21L,"", "jsj", -1,
+                                LocalDateTime.of(2017, Month.FEBRUARY,3,6,30));
+                // employeeController.createEmployee(employee);
+
+                createEmployee(employee);
+
+                List<EmployeeDto> employeesAfter = getAllEmployees();
+
+                assertThat(employeesAfter.subList(0, employeesBefore.size()))
+                        .usingRecursiveFieldByFieldElementComparator()
+                        .containsExactlyElementsOf(employeesBefore);
+
+                assertThat(employeesAfter
+                        .get(employeesAfter.size()-1))
+                        .usingRecursiveComparison()
+                        .isEqualTo(employee);
+
+
+                // Employee employeeTest = employeeService.findById(21L);
+                //assertThat(employeeTest.getSalary()).isEqualTo(200);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        @Test // controllerrel gyártva
+        void testCreateEmployeeInvalidWith_Controller() throws Exception {
+                // EmployeeController employeeController = new EmployeeController();
+                EmployeeDto employee=
+                        new EmployeeDto(21L,"", "jsj", -1,
                                 LocalDateTime.of(2017, Month.FEBRUARY,3,6,30,40,50000));
                 employeeController.createEmployee(employee);
                 Employee employeeTestFromModell = employeeService.findById(21L);
-                assertThat(employeeTestFromModell.getSalary()).isEqualTo(-1);
+                assertThat(employeeTestFromModell.getSalary()).isNotEqualTo(-1);
 
         }
 
