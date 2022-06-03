2e16bd69e112d44625cd5b6b79d56d69b6d9a239
AreYouScorpio <akos.schweger@gmail.com>
2022-06-03 15:35:36 +0200

    IT tests done but validation not working

--- src/test/java/hu/webuni/hr/akostomschweger/web/EmployeeControllerIT.java
+++ src/test/java/hu/webuni/hr/akostomschweger/web/EmployeeControllerIT.java
@@ -25,9 +25,11 @@ public class EmployeeControllerIT {
         @Autowired
         EmployeeController employeeController;
 
+        // ez csak egy plusz teszt a Service-en át --->
 
         @Test
         void testCreateEmployeeValidwithEmployeeService() throws Exception {
+                // service-vel gyártva, Service teszt
                 Employee employee=
                         new Employee(21L,"Akos", "jsj", 200,
                                 LocalDateTime.of(2017, Month.FEBRUARY,3,6,30,40,50000));
@@ -37,15 +39,56 @@ public class EmployeeControllerIT {
 
         }
 
-        @Test
+        // itt a post / create tesztek Controlleren át --->
+
+        @Test // controllerrel gyártva
         void testCreateEmployeeValidWithController() throws Exception {
-                EmployeeController employeeController = new EmployeeController();
+                // EmployeeController employeeController = new EmployeeController();
                 EmployeeDto employee=
                         new EmployeeDto(21L,"Akos", "jsj", 200,
                                 LocalDateTime.of(2017, Month.FEBRUARY,3,6,30,40,50000));
                 employeeController.createEmployee(employee);
-                EmployeeDto employeeTest = employeeController.getById(21L);
+                Employee employeeTest = employeeService.findById(21L);
                 assertThat(employeeTest.getSalary()).isEqualTo(200);
+
+        }
+
+        @Test // controllerrel gyártva
+        void testCreateEmployeeInvalidWithController() throws Exception {
+                // EmployeeController employeeController = new EmployeeController();
+                EmployeeDto employee=
+                        new EmployeeDto(21L,"", "jsj", 200,
+                                LocalDateTime.of(2017, Month.FEBRUARY,3,6,30,40,50000));
+                employeeController.createEmployee(employee);
+                Employee employeeTestFromModell = employeeService.findById(21L);
+                assertThat(employeeTestFromModell.getSalary()).isEqualTo(-1);
+
+        }
+
+        // itt a put / Modify tesztek Controlleren át--->
+
+        @Test // controllerrel gyártva
+        void testModifyEmployeeValidWithController() throws Exception {
+                // EmployeeController employeeController = new EmployeeController();
+                EmployeeDto employee=
+                        new EmployeeDto(1L,"Dkos", "jsj", 200,
+                                LocalDateTime.of(2017, Month.FEBRUARY,3,6,30,40,50000));
+                employeeController.modifyEmployee(1L, employee);
+                Employee employeeTestFromModell = employeeService.findById(1L);
+                assertThat(employeeTestFromModell.getName()).isEqualTo("Dkos");
+
+        }
+
+        @Test // controllerrel gyártva
+        void testModifyEmployeeInvalidWithController() throws Exception {
+                // EmployeeController employeeController = new EmployeeController();
+                EmployeeDto employee=
+                        new EmployeeDto(1L,"Dkos", "jsj", -1,
+                                LocalDateTime.of(2017, Month.FEBRUARY,3,6,30,40,50000));
+                employeeController.modifyEmployee(1L, employee);
+                Employee employeeTestFromModell = employeeService.findById(1L);
+                System.out.println(employeeTestFromModell.getName());
+                assertThat(employeeTestFromModell.getName()).isEqualTo("Akos");
 
         }
 
