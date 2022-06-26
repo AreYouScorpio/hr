21a4a4f304f34d212ad8a0a766595b8687880b68
AreYouScorpio <akos.schweger@gmail.com>
2022-06-26 15:06:23 +0200

    example prepared, errors tbd

--- src/test/java/hu/webuni/hr/akostomschweger/service/EmployeeServiceIT.java
+++ src/test/java/hu/webuni/hr/akostomschweger/service/EmployeeServiceIT.java
@@ -1,14 +1,19 @@
 package hu.webuni.hr.akostomschweger.service;
 
+import hu.webuni.hr.akostomschweger.dto.CompanyDto;
+import hu.webuni.hr.akostomschweger.dto.EmployeeDto;
 import hu.webuni.hr.akostomschweger.model.Employee;
 import hu.webuni.hr.akostomschweger.model.Position;
 import hu.webuni.hr.akostomschweger.model.Qualification;
 import hu.webuni.hr.akostomschweger.repository.EmployeeRepository;
+import hu.webuni.hr.akostomschweger.web.CompanyController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +33,9 @@ public class EmployeeServiceIT {
     @Autowired
     InitDbService initDbService;
 
+    @Autowired
+    CompanyController companyController;
+
     @BeforeEach
     public void init() {
         initDbService.clearDB();
@@ -39,20 +47,68 @@ public class EmployeeServiceIT {
 
     // employeeService.save(new Employee());
 
+        CompanyDto company = new CompanyDto(
+                "1000", "TestCompany", "Wurzburg", new ArrayList<EmployeeDto>());
+
+        EmployeeDto employeeX =
+                new EmployeeDto("V-Man", "spring_student", 111,
+                        LocalDateTime.of(2020, Month.FEBRUARY, 3, 6, 30));
+
+        EmployeeDto employeeY =
+                new EmployeeDto("X-Man", "sprinq_student", 222,
+                        LocalDateTime.of(2021, Month.FEBRUARY, 3, 6, 30));
+
+        //company.addNewEmployee(employeeX);
+        long savedCompanyIdForTesting = createCompany(company);
+
+        long savedEmployeeIdForTesting1 =         companyController.addEmployeeToCompany(savedCompanyIdForTesting, employeeX).getId();
+
+        long savedEmployeeIdForTesting2 =         companyController.addEmployeeToCompany(savedCompanyIdForTesting, employeeY).getId();
+
+
+
         Employee example = new Employee();
         example.setName("A");
-        example.setPosition(new Position("dsjdksj", Qualification.UNIVERSITY));
-        example.setStartDateAtTheCompany(LocalDateTime.of(2002,2,2,1,1));
+        //example.setPosition(new Position("dsjdksj", Qualification.UNIVERSITY));
+        //example.setStartDateAtTheCompany(LocalDateTime.of(2002,2,2,1,1));
 
-        List<Employee> foundFlights =
+        List<Employee> foundEmployees=
                 this.employeeSuperClass.findEmployeesByExample(example);
 
-        assertThat(foundFlights.stream()
-                .map(Flight::getId)
+        assertThat(foundEmployees.stream()
+                .map(Employee::getId)
                 .collect(Collectors.toList()))
-                .containsExactly(flight1, flight2);
+                .containsExactly(savedEmployeeIdForTesting1, savedEmployeeIdForTesting2);
 
 
+    }
+
+    /*
+    private long createEmployee
+            (String flightNumber, long takeoff, long landing, LocalDateTime dateTime) {
+        return employeeSuperClass.(
+                flightNumber,
+                takeoff,
+                landing,
+                dateTime).getId();
+
+    }
+
+     */
+
+    private long createCompany(CompanyDto company) {
+
+        /*
+        webTestClient
+                .post()
+                .uri(BASE_URI)
+                .bodyValue(company)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+         */
+        return companyController.createCompany(company).getId();
     }
 
 
